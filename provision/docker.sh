faeb4dc2c53aab4fe753d4fe7cc8047be223d2a2
Mike Williams <mdub@dogbiscuit.org>
2014-05-03 16:45:40 +1000

    Get docker to accept TCP connections on port 4243.

--- provision/docker.sh
+++ provision/docker.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
 
+cat <<EOF > /etc/sysconfig/docker
+# /etc/sysconfig/docker
+#
+# Other arguments to pass to the docker daemon process
+# These will be parsed by the sysv initscript and appended
+# to the arguments list passed to docker -d
+
+other_args="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock"
+EOF
+
 rpm --upgrade --force http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 yum install -y docker-io
 chkconfig docker on
