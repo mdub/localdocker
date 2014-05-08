34f291ccff2c8bda968f4e84a309b019eea82e44
Mike Williams <mdub@dogbiscuit.org>
2014-05-08 12:41:42 +1000

    Make docker installation work reliably.

--- provision/docker.sh
+++ provision/docker.sh
@@ -1,9 +1,13 @@
 #! /bin/sh
 
-groupadd docker
+set -e
+
+getent group docker || groupadd docker
 usermod -a -G docker vagrant
 
-dpkg -l lxc-docker > /dev/null || (curl -L http://get.docker.io/ubuntu | sh)
+rm -fr /etc/default/docker
+
+wget -q -O - http://get.docker.io/ubuntu | sh
 
 cat <<EOF > /etc/default/docker
 DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock"
