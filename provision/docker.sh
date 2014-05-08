e1e2e6d4cb2f2105257d2c69a0d162ddfa34e920
Mike Williams <mdub@dogbiscuit.org>
2014-05-08 12:05:59 +1000

    Optimise re-provisioning.

--- provision/docker.sh
+++ provision/docker.sh
@@ -3,8 +3,10 @@
 groupadd docker
 usermod -a -G docker vagrant
 
+dpkg -l lxc-docker > /dev/null || (curl -L http://get.docker.io/ubuntu | sh)
+
 cat <<EOF > /etc/default/docker
 DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock"
 EOF
 
-curl -L http://get.docker.io/ubuntu | sh
+service docker restart
