8bf3e2ce21e17febcf021f24474f28423ae34ca3
Mike Williams <mdub@dogbiscuit.org>
2014-09-02 15:19:19 +1000

    Change the way we install Docker, to ensure AUFS.

--- provision/docker.sh
+++ provision/docker.sh
@@ -5,9 +5,10 @@ set -e
 getent group docker || groupadd docker
 usermod -a -G docker vagrant
 
-rm -fr /etc/default/docker
-
-wget -q -O - http://get.docker.io/ubuntu | sh
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sh -c 'echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list'
+apt-get -y -q update
+apt-get -y -q install lxc-docker linux-image-extra-`uname --kernel-release`
 
 cat <<EOF > /etc/default/docker
 DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock"
